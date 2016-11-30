f1626d4a609dd34f2bc8ec35d8afb8a125789f3f
Anne Belka and Brian Pritt <anne+brian@>
2016-11-30 11:36:36 -0800

    add user interface

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,18 +1,29 @@
-// using Nancy;
-// using System.Collections.Generic;
-// using System;
-// using TEMPLATE.Objects;
-//
-// namespace GameSpace
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] = _ => {
-//         return View["TEMPLATE.cshtml"];
-//       };
-//
-//     }
-//   }
-// }
+using Nancy;
+using System.Collections.Generic;
+using System;
+using GameSpace.Objects;
+
+namespace GameSpace
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+
+        return View["index.cshtml"];
+      };
+      Post["/new-game"] =_=> {
+        string playerOne = Request.Form["playerOne"];
+        string playerTwo = Request.Form["playerTwo"];
+        Game newGame = new Game(playerOne, playerTwo);
+
+        Console.WriteLine(playerOne);
+        Console.WriteLine(playerTwo);
+        return View["results.cshtml", newGame];
+      };
+
+
+    }
+  }
+}
